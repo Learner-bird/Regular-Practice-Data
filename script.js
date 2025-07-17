6058d2773557e040ee4546e1f364215c462c35df
learner_bird <smmexpert381@gmail.com>
2025-07-18 01:45:07 +0600

    All Data has been updated

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 function dataFetch(){
+    const container = document.querySelector('.data-container')
+    console.log(container)
 
     fetch('https://jsonplaceholder.typicode.com/posts')
     .then(response => response.json())
-    .then(json => console.log(json))
     .then(data => {
-        console.log(data);
+        data.forEach(element => {
+
+            const idDiv = document.createElement("div")
+            idDiv.innerText = element.id
+
+            const titlediv = document.createElement("div")
+            titlediv.innerText = element.title
+
+            const bodyDiv = document.createElement("div")
+            bodyDiv.innerText = element.body
+
+
+           container.appendChild(idDiv)
+           container.appendChild(titlediv)
+           container.appendChild(bodyDiv)
+
+        }); 
     })
 }
