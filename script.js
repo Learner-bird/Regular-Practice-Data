62b8ef6b4a9854db537626760a6b10ca4c4835b6
learner_bird <smmexpert381@gmail.com>
2025-07-18 01:46:44 +0600

    all data has been changed

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
-function dataFetch(){
+function dataFetch() {
     const container = document.querySelector('.data-container')
     console.log(container)
 
     fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(response => response.json())
-    .then(data => {
-        data.forEach(element => {
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(element => {
 
-            const idDiv = document.createElement("div")
-            idDiv.innerText = element.id
+                const postDiv = document.createElement("div")
+                postDiv.className ="post"
+                const idDiv = document.createElement("div")
+                const titlediv = document.createElement("div")
+                const bodyDiv = document.createElement("div")
 
-            const titlediv = document.createElement("div")
-            titlediv.innerText = element.title
+               
+                idDiv.innerText = element.id
+                titlediv.innerText = element.title
+                bodyDiv.innerText = element.body
 
-            const bodyDiv = document.createElement("div")
-            bodyDiv.innerText = element.body
+                container.appendChild(postDiv)
+                postDiv.appendChild(idDiv)
+                postDiv.appendChild(titlediv)
+                postDiv.appendChild(bodyDiv)
 
 
-           container.appendChild(idDiv)
-           container.appendChild(titlediv)
-           container.appendChild(bodyDiv)
-
-        }); 
-    })
+            });
+        })
 }
